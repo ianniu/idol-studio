778f773cddef3a6bd516998efe02f2ca050070b6
Yiheng Niu <niuyiheng@Ians-MacBook-Pro.local>
2022-12-09 18:03:14 -0800

    fix: show weather only when it exists

--- components/LocationManager.js
+++ components/LocationManager.js
@@ -1,14 +1,14 @@
 import { View, Text, Image, Button, StyleSheet } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import * as Location from 'expo-location'
-import { Colors } from "../styles/Styles"
-import { mapAPIKey, weatherAPIKey } from "@env"
+import { Colors } from '../styles/Styles'
+import { mapAPIKey, weatherAPIKey } from '@env'
 
 export default function LocationManager() {
   const [permissionResponse, requestPermission] = Location.useForegroundPermissions()
   const [location, setLocation] = useState()
   const [weather, setWeather] = useState({})
-  const [description, setDescription] = useState("--")
+  const [description, setDescription] = useState('--')
 
   const verifyPermission = async () => {
     if (permissionResponse.granted) {
@@ -25,32 +25,44 @@ export default function LocationManager() {
         return
       }
       const currentLocation = await Location.getCurrentPositionAsync()
-      setLocation({latitude: currentLocation.coords.latitude, longitude: currentLocation.coords.longitude})
+      setLocation({
+        latitude: currentLocation.coords.latitude,
+        longitude: currentLocation.coords.longitude
+      })
       fetch(
         `https://api.openweathermap.org/data/2.5/onecall?lat=${currentLocation.coords.latitude}&lon=${currentLocation.coords.longitude}&exclude=hourly,minutely&units=metric&appid=${weatherAPIKey}`
       )
         .then((res) => res.json())
         .then((data) => {
           setWeather(data.current)
-          if (weather) {
-            setDescription(weather.weather[0].description)
-          }
         })
         .catch((err) => {
-          console.log("error", err)
+          console.log('error', err)
         })
     } catch (err) {
       console.log(err)
     }
   }
 
+  useEffect(() => {
+    if (!weather || !weather.weather || !weather.weather[0]) return
+    setDescription(weather.weather[0].description)
+  }, [weather])
+
   return (
     <View>
-      <Button title='Locate Me and Show Weather' onPress={locateUserHandler}/>
+      <Button title="Locate Me and Show Weather" onPress={locateUserHandler} />
       {location && (
         <>
-          <Image source={{ uri: `https://maps.googleapis.com/maps/api/staticmap?center=${location.latitude},${location.longitude}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:L%7C${location.latitude},${location.longitude}&key=${mapAPIKey}` }} style={{ width: 300, height: 200 }} />
-          <Text style={styles.text}>Temperature: {weather.temp}°C</Text>
+          <Image
+            source={{
+              uri: `https://maps.googleapis.com/maps/api/staticmap?center=${location.latitude},${location.longitude}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:L%7C${location.latitude},${location.longitude}&key=${mapAPIKey}`
+            }}
+            style={{ width: 300, height: 200 }}
+          />
+          {weather && (
+            <Text style={styles.text}>Temperature: {weather.temp ? weather.temp : ' '}°C</Text>
+          )}
           <Text style={styles.text}>Weather: {description}</Text>
         </>
       )}
@@ -62,6 +74,6 @@ const styles = StyleSheet.create({
   text: {
     color: Colors.white1,
     fontSize: 24,
-    textAlign: "center",
-  },
+    textAlign: 'center'
+  }
 })
